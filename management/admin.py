7d632c2325b0abae10c7b26fe47c46d6ea8a23bc
jakubharan1 <jakub.haran02@gmail.com>
2026-02-18 23:45:06 +0100

    RegisterApiView and working with permissions

--- management/admin.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-from .models import UserModel, Property, Lease
-
-admin.site.register(UserModel)
-admin.site.register(Property)
-admin.site.register(Lease)
-
-# Register your models here.
